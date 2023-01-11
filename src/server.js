767e51b3552f7c971b079d563e8070ef19d96453
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-10 21:17:25 -0300

    feat: add http routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,21 @@ import http from 'node:http'
 const PORT = 3333
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+
+  const isGetMethod = method === 'GET'
+  const isPostMethod = method === 'POST'
+
+  const hasUserPath = url === '/users'
+
+  if (isGetMethod && hasUserPath) {
+    return response.end('List user')
+  }
+
+  if (isPostMethod && hasUserPath) {
+    return response.end('Create user')
+  }
+
   return response.end('Hello World')
 })
 
