98455df86e48b7886d1b51dbadd53d2c15704d3b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-12 13:38:21 -0300

    refactor: add json middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import http from 'node:http'
 
+import { json } from './middlewares/json.js'
+
 const PORT = 3333
 
 const users = []
@@ -12,20 +14,10 @@ const server = http.createServer(async (request, response) => {
 
   const hasUserPath = url === '/users'
 
-  const buffers = []
-
-  for await (const chunk of request) {
-    buffers.push(chunk)
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    request.body = null
-  }
+  await json(request, response)
 
   if (isGetMethod && hasUserPath) {
-    return response.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+    return response.end(JSON.stringify(users))
   }
 
   if (isPostMethod && hasUserPath) {
