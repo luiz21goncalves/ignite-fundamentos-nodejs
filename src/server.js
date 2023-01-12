11b1f6da8d5fb3c7e1dd1871aa7cf4e96d1a0291
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-11 21:32:28 -0300

    chore: parse the resquest body in json

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const PORT = 3333
 
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request
 
   const isGetMethod = method === 'GET'
@@ -12,15 +12,29 @@ const server = http.createServer((request, response) => {
 
   const hasUserPath = url === '/users'
 
+  const buffers = []
+
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    request.body = null
+  }
+
   if (isGetMethod && hasUserPath) {
     return response.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
   }
 
   if (isPostMethod && hasUserPath) {
+    const { name, email } = request.body
+
     users.push({
       id: users.length + 1,
-      name: 'John Doe',
-      email: 'johndoe@email.com'
+      name,
+      email
     })
 
     return response.writeHead(201).end()
