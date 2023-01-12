70707d85239b16f378b50e38cef6a315b06ae432
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-01-12 16:24:39 -0300

    refactor: add routes file

--- src/server.js
+++ src/server.js
@@ -1,41 +1,21 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
 const PORT = 3333
-
-const database = new Database()
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request
 
-  const isGetMethod = method === 'GET'
-  const isPostMethod = method === 'POST'
-
-  const hasUserPath = url === '/users'
-
   await json(request, response)
 
-  if (isGetMethod && hasUserPath) {
-    const users = database.select('users')
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url
+  })
 
-    return response.end(JSON.stringify(users))
-  }
-
-  if (isPostMethod && hasUserPath) {
-    const { name, email } = request.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
-
-    database.insert('users', user)
-
-    return response.writeHead(201).end()
+  if (route) {
+    return route.handler(request, response)
   }
 
   return response.writeHead(404).end()
